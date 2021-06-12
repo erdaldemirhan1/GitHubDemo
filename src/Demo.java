74eadf8ea9fd52f9f937f9a3a7e28cb9d1ef9808
erdaldemirhan1 <eerdaldemirhan@gmail.com>
2021-06-12 12:48:53 -0400

    hh

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,11 @@ public class Demo {
 
     //git init
     //git status(To see the status)
-    //git add (The file you will add)
+    //git add (The file you will add)git branch -M main
+
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+    }
 
 
 
