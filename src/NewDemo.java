846f344d5af4df9eaeffd8cbd40dc64a76ab7de6
erdaldemirhan1 <eerdaldemirhan@gmail.com>
2021-06-14 19:29:09 -0400

    New Github Example

--- src/NewDemo.java
+++ src/NewDemo.java
@@ -1,2 +1,9 @@
 public class NewDemo {
+
+    public static void main(String[] args) {
+        int x=8;
+        if (x==8){
+            System.out.println("New GitHub Example");
+        }
+    }
 }
